3233e47e377b031cf2d2612352f1c2b49589c24d
netobjj <joseeuclides18@gmail.com>
2023-07-16 21:55:13 -0300

    initial_commit

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,31 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "*"
+    }
+});
+const cors = require('cors');
 
+app.use(cors())
 
 app.get('/', (req, res)=> {
     res.send('<h1>Hello World</h1>');
 });
 
 http.listen(3000, ()=>{
-    console.log('listning on port 3000');
+    console.log('Servidor rodando na porta 3000');
 });
 
 io.on('connection', (socket) =>{
-    console.log('Client connected');
+    console.log('Cliente conectado!');
     dataUpdate(socket);
 });
 
 function dataUpdate(socket) {
-    socket.emit('dataupdate', Array.from({length: 8}, ()=> Math.floor(Math.random() * 40)));
+    socket.emit('dataupdate', "Clube 'Louvadores' da ACM chegou!" + new Date().toLocaleString())//Array.from({length: 8}, ()=> Math.floor(Math.random() * 40)));
 
     setTimeout(() =>{
         dataUpdate(socket);
-    }, 2000)
+    }, 5000)
 }
