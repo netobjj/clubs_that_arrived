bf9c35243e2c6c1450c2e8026f6a080d6ca80129
netobjj <joseeuclides18@gmail.com>
2023-07-19 00:54:05 -0300

    att_chart

--- frontend/src/app/chart-associations/chart-associations.component.ts
+++ frontend/src/app/chart-associations/chart-associations.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 import { single } from '../../../../backend/data_chart';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { Color, ScaleType } from '@swimlane/ngx-charts';
+import { Color, ColorHelper, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'club-arrived-chart-associations',
@@ -10,32 +10,47 @@ import { Color, ScaleType } from '@swimlane/ngx-charts';
   styleUrls: ['./chart-associations.component.css']
 })
 export class ChartAssociationsComponent {
-  //@Input() name: string = '';
-  //@Input() association: string = '';
+  @Input() name: string = '';
+  @Input() association: string = '';
   @Input() canShow: boolean = false;
 
   single: any[];
   view: any = [700, 400]; // Dimensões do gráfico
   showXAxis = true; // Exibir eixo X
-  showYAxis = true; // Exibir eixo Y
+  showYAxis = false; // Exibir eixo Y
   showXAxisLabel = true; // Exibir rótulo do eixo X
-  showYAxisLabel = true; // Exibir rótulo do eixo Y
-  xAxisLabel = 'Países'; // Rótulo do eixo X
+  showYAxisLabel = false; // Exibir rótulo do eixo Y
+  xAxisLabel = 'Associações'; // Rótulo do eixo X
   yAxisLabel = 'População'; // Rótulo do eixo Y
   colorScheme: Color = {
     name: 'myScheme',
-    selectable: true,
-    group: ScaleType.Ordinal,
-    domain: ['#f00', '#0f0', '#0ff'],
-  }; // Cores das barras
+    selectable: false,
+    group: ScaleType.Quantile,
+    domain: ["#0f0"]
+  };
+
+  animations = true;
   gradient = false; // Habilitar gradiente nas barras
 
   constructor() {
-    this.single = single; // Dados para o gráfico
+    Object.assign(this, { single }); // Dados para o gráfico
   }
 
-  ngOnInit(): void {
-    
-  }
-  
+  ngOnInit(): void { }
+
+  /* barCustomColors() {
+    let result: any[] = [];
+    for (let i = 0; i < this.single.length; i++) {
+      if (this.single[i].value <= 30) {
+        result.push({ "name": this.single[i].name, "value": "#f00" }); // Red
+      } else if (this.single[i].value > 30 && this.single[i].value <= 60) {
+        result.push({ "name": this.single[i].name, "value": "#ff0" }); // Yellow
+      } else if (this.single[i].value > 60 && this.single[i].value <= 99) {
+        result.push({ "name": this.single[i].name, "value": "#00f" }); // blue
+      } else if (this.single[i].value > 99) {
+        result.push({ "name": this.single[i].name, "value": "#0f0" }); // green
+      }
+    }
+    return result;
+  } */
 }
